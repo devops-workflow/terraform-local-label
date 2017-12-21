90a21608a0566c3672e79980556e9d3283fd333a
Steven Nemetz <snemetz@wiser.com>
2017-12-21 10:06:25 -0800

    Replace `_` with `-` in name instead of just trucated IDs

--- main.tf
+++ main.tf
@@ -25,13 +25,13 @@ module "namespace-org" {
 locals {
   attr        = "${lower(format("%s", join(var.delimiter, compact(var.attributes))))}"
   env         = "${lower(format("%s", var.environment))}"
-  name_low    = "${lower(format("%s", var.name))}"
+  name_low    = "${replace(lower(format("%s", var.name)),"_","-")}"
   org         = "${lower(format("%s", var.organization))}"
   id_env      = "${module.namespace-env.value ? join(var.delimiter, list(local.env, local.name_low)) : local.name_low}"
   id_org      = "${module.namespace-org.value ? join(var.delimiter, list(local.org, local.id_env)) : local.id_env}"
   id          = "${length(local.attr) > 0 ? join(var.delimiter, list(local.id_org, local.attr)) : local.id_org}"
-  id_20       = "${substr(replace(local.id,"_","-"),0,19 <= length(local.id) ? 19 : length(local.id))}"
-  id_32       = "${substr(replace(local.id,"_","-"),0,31 <= length(local.id) ? 31 : length(local.id))}"
+  id_20       = "${substr(local.id,0,19 <= length(local.id) ? 19 : length(local.id))}"
+  id_32       = "${substr(local.id,0,31 <= length(local.id) ? 31 : length(local.id))}"
   org_attr_20 = "${min(18 - length(local.attr), length(local.id_org))}"
   id_attr_20  = "${19 <= length(local.id) ?
     join(var.delimiter,
