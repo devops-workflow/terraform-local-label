a063de14b18fde42c1a5821bba9213959e23118f
Steven Nemetz <snemetz@wiser.com>
2018-05-17 10:08:07 -0700

    Fix formatting

--- examples/attributes/main.tf
+++ examples/attributes/main.tf
@@ -1,7 +1,7 @@
 module "labels" {
-  source            = "../../"
-  name              = "CapMe"
-  environment       = "Dev"
-  organization      = "CorpXyZ"
-  attributes        = ["role", "policy", "use", ""]
+  source       = "../../"
+  name         = "CapMe"
+  environment  = "Dev"
+  organization = "CorpXyZ"
+  attributes   = ["role", "policy", "use", ""]
 }
